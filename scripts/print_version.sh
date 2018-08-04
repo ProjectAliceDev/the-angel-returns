ce856043b8e32bed2b2ae3077bee2c0389c169bb
Capuccino <chinodesuuu@gmail.com>
2018-08-04 13:14:26 +0000

    use Git branch for version

--- scripts/print_version.sh
+++ scripts/print_version.sh
@@ -4,7 +4,8 @@ if [[ -z "$CI" ]] && [[ -z "$TRAVIS_TAG" ]]; then
   echo "{\"version\":\"$TRAVIS_TAG\"}" >> version;
   exit 0;
 elif [[ ! -z "$TRAVIS_TAG" ]]; then
-  echo "No Git tag detected. Exiting.";
+  echo "No Git tag detected. Using Git branch.";
+  echo "{\"version\":\"$(git rev-parse --abbrev-ref HEAD)\"}" >> version;
   exit 0;
 else
   echo "Not in CI! Version is gonna be printed out using Git branch.";
