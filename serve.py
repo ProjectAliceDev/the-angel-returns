4957acbce5033cdbaa6f4bfe662f8e16371da689
Marquis Kurt <software@marquiskurt.net>
2018-08-04 08:33:24 -0400

    Remove serve script (replaced by make)

--- serve.py
+++
@@ -1,7 +0,0 @@
-## Python Serve Script
-import os
-get_time = "something"
-with open("version.py", 'w+') as st:
-    st.write("init python:")
-    st.write("    snapshottime = " + get_time)
-os.system("./renpy.sh "../mod/" lint && ./renpy.sh launcher distribute "../mod/" && cd ..")
