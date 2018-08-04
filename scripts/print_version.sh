3beb73607b53ed9ad161420339587d5fed565ab0
Capuccino <chinodesuuu@gmail.com>
2018-08-05 07:55:06 +0800

    Update print_version.sh

--- scripts/print_version.sh
+++ scripts/print_version.sh
@@ -3,12 +3,8 @@
 if [[ -z "$CI" ]] && [[ -z "$TRAVIS_TAG" ]]; then
   echo "{\"version\":\"$TRAVIS_TAG\"}" >> version;
   exit 0;
-elif [[ ! -z "$TRAVIS_TAG" ]]; then
-  echo "No Git tag detected. Using Git branch.";
-  echo "{\"version\":\"$(git rev-parse --abbrev-ref HEAD)\"}" >> version;
-  exit 0;
 else
-  echo "Not in CI! Version is gonna be printed out using Git branch.";
+  echo "Not a Release - Version will be printed out using Git branch.";
   echo "{\"version\":\"$(git rev-parse --abbrev-ref HEAD)\"}" >> version;  
   exit 0;
 fi
