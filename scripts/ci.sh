1f131943e91d676855e22c095f27c9dbbde5cdaa
Capuccino <chinodesuuu@gmail.com>
2018-08-05 05:50:14 +0000

    revert travis script
    we're not using it atm

--- scripts/ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if ! [ -z "$TRAVIS_TAG" ]; then 
-  echo ">>>>>>>>>>>>>>>>>>> Starting Build in Release Mode. <<<<<<<<<<<<<<<<<<<<<<<"
-  make dev -f "$(pwd)/the-angel-returns/Makefile" -C "$(pwd)/the-angel-returns/";
-else
-  echo ">>>>>>>>>>>>>>>>>>> Starting Build in Developer Mode. <<<<<<<<<<<<<<<<<<<<<<<"
-  make release -f "$(pwd)/the-angel-returns/Makefile" -C "$(pwd)/the-angel-returns/";
-fi
