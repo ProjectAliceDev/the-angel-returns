8f4691a7b0a249772513352299ce9b3f014c41f9
Capuccino <chinodesuuu@gmail.com>
2018-08-06 04:54:13 +0000

    finish up script

--- autobuild.sh
+++ autobuild.sh
@@ -14,27 +14,85 @@ echo " GitHub: https://github.com/Sayo-nika/autobuild.sh"
 echo " Bug reports : https://github.com/Sayo-nika/autobuild.sh/issues/new"
 echo ""
 
+# This pulls from the Sayonika-maintained S3 Storage for the DDLC base content.
+# It only contains the RPAs required to build a mod.
+
+pull_ddlc_base() {
+    mc_endpoint="https://s3-api.us-geo.objectstorage.softlayer.net"
+    mc_hmac_key="aa1d6f56b97443c185d7282c22adc4a7"
+    mc_hmac_secret="29fc312082d26720ceeec6e89630f6d2fc382a96c7a72b1c"
+    mc_alias="sayonika"
+    mc_bucket="filepub"
+    mc_filename="ddlc_pkg.zip"
+    
+    echo "Checking if Minio S3 is present to pull DDLC resources."
+    
+    if [ -z "$(command -v mc)" ]; then
+      echo "Minio Client not present. Installing Minio S3 Client"
+      wget "https://dl.minio.io/client/mc/release/linux-amd64/mc" -O $DIRECTORY/build/mc && \
+      chmod +x mc && \
+      export PATH="$DIRECTORY/build:$PATH" && \
+      $DIRECTORY/build/mc config host add $mc_alias $mc_endpoint $mc_hmac_key $mc_hmac_secret && \
+      $DIRECTORY/build/mc ls $mc_alias;
+      $DIRECTORY/build/mc cp "$mc_alias/$mc_bucket/$mc_filename" $DIRECTORY/build/
+      unzip  $mc_filename -d $DIRECTORY/build/mod/game
+    elif [ -f "$DIRECTORY/build/mc" ]; then
+      echo "Minio Client present in build. Exporting to PATH."
+      export PATH="$DIRECTORY/build:$PATH" && \
+      $DIRECTORY/build/mc config host add $mc_alias $mc_endpoint $mc_hmac_key $mc_hmac_secret && \
+      $DIRECTORY/build/mc ls $mc_alias;
+      $DIRECTORY/build/mc cp "$mc_alias/$mc_bucket/$mc_filename" $DIRECTORY/build/
+      unzip  $mc_filename -d $DIRECTORY/build/mod/game
+    else 
+      echo "Minio Client exists or Midnight Commander is present."
+      echo "Make sure Midnight Commander isn't installed since it causes issues with this script."
+      $DIRECTORY/build/mc config host add $mc_alias $mc_endpoint $mc_hmac_key $mc_hmac_secret && \
+      # try if it works
+      $DIRECTORY/build/mc ls $mc_alias;
+      $DIRECTORY/build/mc cp "$mc_alias/$mc_bucket/$mc_filename" $DIRECTORY/build/
+      unzip  $mc_filename -d $DIRECTORY/build/mod/game
+    fi
+}
+
+# DDTAR-specific Autobuild function.
+# This won't be included in the mainline autobuild.sh
+print_version() {
+    if [ ! -f "$DIRECTORY/build/mod/version" ]; then
+      echo "Printing Versionfile for mod. Make sure your mod's package version file has output like this."
+      touch $DIRECTORY/build/mod/version
+      echo "{\"version\":\"$(printf "local")\"}" | tee $DIRECTORY/build/mod/version
+    else
+      echo "Skipping Version File creation."
+    fi
+}
+
 regex='(https?|ftp|file)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
 
-read -p "Enter your mod's Location: " input
+read -p "Enter your mod's Location (use . if you have this script inside your mod folder): " input
 
 # Really needed Type Checks
 
 while [[ $input =~ $regex || -z $input ]] ; do
   echo "Invalid input. Try again."
-  read -p "Enter your mod's Location: " input
+  read -p "Enter your mod's Location (use . if you have this script inside your mod folder): " input
 done
 
 while [[ ! -d $input ]] ; do
   echo "Directory does not exist. Try a different directory."
-  read -p "Enter your mod's Location: " input
+  read -p "Enter your mod's Location (use . if you have this script inside your mod folder): " input
 done
 
-DIRECTORY="$input"
 
-echo "Building Mod in $DIRECTORY"
-echo "If you have this builder script inside your own project folder, make sure you input your folder as ../FOLDERNAME or use '.'."
-echo "Thanks, and have a nice day!"
+if [[ $input == '.' ]]; then
+  echo "Building mod in your PWD context."
+  echo "Do you know you can also build other mods with this? Just type the absolute path of the mod and enter. Happy Modding!"
+  DIRECTORY="$(pwd)"
+ else
+  echo "Building Mod in $DIRECTORY"
+  echo "If you have this builder script inside your own project folder, make sure you input your folder as ../FOLDERNAME or use '.'."
+  DIRECTORY="$input"
+fi
+
 
 sleep 3;
 
@@ -57,13 +115,14 @@ if [[ -d "$DIRECTORY/build" ]]; then
           rm renpy-6.99.12.4-sdk.tar.bz2
           mv renpy-6.99.12.4-sdk renpy
           rm -rf renpy-6.99.12.4-sdk
-          pull_ddlc_base;
+          cd $DIRECTORY/build && pull_ddlc_base;
           print_version;
           cd $DIRECTORY/build/renpy 
           ./renpy.sh "$DIRECTORY/build/mod/" lint && ./renpy.sh launcher distribute "$DIRECTORY/build/mod/"$1
           cd ..
        else
           mkdir -p $DIRECTORY/build
+          mkdir -p $DIRECTORY/build/mod
           cp -vRf $DIRECTORY/* $DIRECTORY/build/mod
           cd $DIRECTORY
           wget https://www.renpy.org/dl/6.99.12.4/renpy-6.99.12.4-sdk.tar.bz2
@@ -71,57 +130,34 @@ if [[ -d "$DIRECTORY/build" ]]; then
           rm renpy-6.99.12.4-sdk.tar.bz2
           mv renpy-6.99.12.4-sdk renpy
           rm -rf renpy-6.99.12.4-sdk
-          pull_ddlc_base;
+          cd $DIRECTORY/build && pull_ddlc_base;
           print_version;
           cd $DIRECTORY/build/renpy
           ./renpy.sh "$DIRECTORY/build/mod/" lint && ./renpy.sh launcher distribute "$DIRECTORY/build/mod/"$1
           cd ..
        fi
     fi
+else 
+      echo "Looks like it's your first time building this mod. Here, I'll make it up to you~!"
+      mkdir -p $DIRECTORY/build
+      mkdir -p $DIRECTORY/build/mod
+      cp -vRf $DIRECTORY/* $DIRECTORY/build/mod
+      cd $DIRECTORY
+      wget https://www.renpy.org/dl/6.99.12.4/renpy-6.99.12.4-sdk.tar.bz2
+      tar xf renpy-6.99.12.4-sdk.tar.bz2
+      rm renpy-6.99.12.4-sdk.tar.bz2
+      mv renpy-6.99.12.4-sdk renpy
+      rm -rf renpy-6.99.12.4-sdk
+      cd $DIRECTORY/build && pull_ddlc_base;
+      print_version;
+      cd $DIRECTORY/build/renpy
+      ./renpy.sh "$DIRECTORY/build/mod/" lint && ./renpy.sh launcher distribute "$DIRECTORY/build/mod/"$1
+      cd ..
 fi
 
 case $(exit $?) in 
   0) echo "Build Successfully made. Find it at $DIRECTORY/build/ModXY-dists or similar. Happy modding!" && exit 0;
    ;;
-  1) echo "Uh oh, we can't build your mod in $DIRECTORY. If this is a mistake, file a issue. Thank you." && exit 1;
-   ;;
-  *) echo "We had a error we don't know. Exiting." && exit 1;
+  *) echo "Uh oh, we can't build your mod in $DIRECTORY. If this is a mistake, file a issue. Thank you." && exit 1;
    ;;
 esac
-
-# This pulls from the Sayonika-maintained S3 Storage for the DDLC base content.
-# It only contains the RPAs required to build a mod.
-
-pull_ddlc_base() {
-    mc_endpoint="https://s3-api.us-geo.objectstorage.softlayer.net"
-    mc_hmac_key="aa1d6f56b97443c185d7282c22adc4a7"
-    mc_hmac_secret="29fc312082d26720ceeec6e89630f6d2fc382a96c7a72b1c"
-    mc_alias="sayonika"
-    mc_bucket="filepub"
-    mc_filename="ddlc_pkg.zip"
-    
-    echo "Checking if Minio S3 is present to pull DDLC resources."
-    
-    if ! [-z "$(command -v mc)"]; then
-      echo "Minio Client exists or Midnight Commander is present."
-      echo "Make sure Midnight Commander isn't installed since it causes issues with this script."
-      mc config host add $mc_alias $mc_endpoint $mc_hmac_key $mc_hmac_secret && \
-      # try if it works
-      mc ls $mc_alias;
-    else 
-      echo "Minio Client not present. Installing Minio S3 Client"
-      wget "https://dl.minio.io/client/mc/release/linux-amd64/mc" && \
-      chmod +x mc && \
-      export PATH="$DIRECTORY/build:$PATH" && \
-      mc config host add $mc_alias $mc_endpoint $mc_hmac_key $mc_hmac_secret && \
-      mc ls $mc_alias;
-    fi
-}
-
-print_version() {
-    if [ ! -f "$DIRECTORY/build/mod/version" ]; then
-      echo "{\"version\":\"$(printf "local")\"}" | tee $DIRECTORY/build/mod/version
-    else
-      echo "Skipping Version File creation."
-    fi
-}
